4326c71eaa4cfc2afbba5bee96cc887bb21de8dc
Ankit Jasuja <AJasuja@walmartlabs.com>
2016-02-16 21:54:44 -0800

    initial commit

---
+++ src/main/java/com/whitepages/dungeon/PlaceHolder.java
@@ -0,0 +1,5 @@
+package com.whitepages.dungeon;
+
+public class PlaceHolder {
+
+}
---
+++ src/test/java/com/whitepages/dungeon/PlaceHolderTest.java
@@ -0,0 +1,12 @@
+package com.whitepages.dungeon;
+
+import org.junit.Test;
+
+public class PlaceHolderTest {
+
+	@Test
+	public void test1() {
+		System.out.println("placeholder test");
+	}
+
+}
